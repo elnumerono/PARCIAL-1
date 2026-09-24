801685974272ccff32c220ecac00608b90f673f5
Oscar Gabriel Eraso Torres <oscarg.erasot@uqvirtual.edu.co>
2026-09-24 17:00:49 -0500

    Hice los metodos de la clase Hotel, para gestionar los Huespedes y las Habitaciones

--- src/model/Hotel.java
+++ src/model/Hotel.java
@@ -35,4 +35,79 @@ public class Hotel {
         this.listHotelHabitaciones = new ArrayList<>();
         this.listHotelReservas = new ArrayList<>();
     }
+    //GESTIÓN DE HUÉSPEDES
+
+    /**
+     * Metodo para buscar Huesped
+     * @param documento del huesped
+     * @return
+     */
+    public Huesped buscarHuesped(String documento) {
+        for (Huesped h : listHotelHuespedes) {
+            if (h.getDocumentoIdentidad().equals(documento)) return h;
+        }
+        return null;
+    }
+
+    /**
+     * Metodo para buscar huesped por telefono
+     * @param tel del huesped
+     * @return
+     */
+
+    public Huesped buscarHuespedPorTelefono(int tel) {
+        for (Huesped h : listHotelHuespedes) {
+            if (h.getTelefono() == tel) return h;
+        }
+        return null;
+    }
+
+    /**
+     * Metodo para registrar huesped
+     * @param nombre del huesped
+     * @param doc del huesped
+     * @param tel del huesped
+     * @param email del huesped
+     * @param pais del huesped
+     * @return
+     */
+    public boolean registrarHuesped(String nombre, String doc, int tel, String email, String pais) {
+        if (buscarHuesped(doc) == null) {
+            listHotelHuespedes.add(new Huesped(nombre, doc, tel, email, pais));
+            return true;
+        }
+        return false;
+    }
+
+    //GESTIÓN DE HABITACIONES
+
+    /**
+     * Metodo para buscar habitacion
+     * @param numero de la habitacion
+     * @return
+     */
+    public Habitacion buscarHabitacion(int numero) {
+        for (Habitacion h : listHotelHabitaciones) {
+            if (h.getNumero() == numero) return h;
+        }
+        return null;
+    }
+
+    /**
+     * Metodo para registrar habitacion
+     * @param numero de la habitacion
+     * @param piso de la habitacion
+     * @param tipo de la habitacion
+     * @param capacidad de la habitacion
+     * @param precio de la habitacion
+     * @param estado de la habitacion
+     * @return
+     */
+    public boolean registrarHabitacion(int numero, int piso, String tipo, int capacidad, double precio, String estado) {
+        if (buscarHabitacion(numero) == null) {
+            listHotelHabitaciones.add(new Habitacion(numero, piso, tipo, capacidad, precio, estado));
+            return true;
+        }
+        return false;
+    }
 }
