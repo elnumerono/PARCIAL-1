c47c787700915ebc280a97015e5fbbfda69e7fda
Oscar Gabriel Eraso Torres <oscarg.erasot@uqvirtual.edu.co>
2026-09-24 16:53:53 -0500

    Complete la clase Habitacion con los getters, setters, el to-String y el metodo para cambiar el estado de la Habitacion, falta completas Huesped

--- src/model/Habitacion.java
+++ src/model/Habitacion.java
@@ -24,5 +24,76 @@ public class Habitacion {
         this.precioPorNoche = precioPorNoche;
         this.estado = estado;
     }
+    /**
+     * Metodo para cambiar Estado
+     * @param nuevoEstado de habitacion
+     */
+    public void cambiarEstado(String nuevoEstado) {
+        this.estado = nuevoEstado;
+    }
+
+    /**
+     * Metodo toString
+     * @return
+     */
+    @Override
+    public String toString() {
+        return "Habitacion{" +
+                "numero=" + numero +
+                ", piso=" + piso +
+                ", capacidad=" + capacidad +
+                ", tipo='" + tipo + '\'' +
+                ", estado='" + estado + '\'' +
+                ", precioPorNoche=" + precioPorNoche +
+                '}';
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getPiso() {
+        return piso;
+    }
+
+    public void setPiso(int piso) {
+        this.piso = piso;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public double getPrecioPorNoche() {
+        return precioPorNoche;
+    }
+
+    public void setPrecioPorNoche(double precioPorNoche) {
+        this.precioPorNoche = precioPorNoche;
+    }
 
 }
